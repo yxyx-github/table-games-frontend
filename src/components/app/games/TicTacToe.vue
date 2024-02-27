c2805a992516165f1be4a95c8cc3c1a3fa34a634
Marvin Waldmann <marvin.waldmann@sap.com>
2024-02-27 11:04:53 +0100

    GameField component added

--- src/components/app/games/TicTacToe.vue
+++ src/components/app/games/TicTacToe.vue
@@ -1,6 +1,14 @@
 <template>
     TTT
+    <GameField :fields="fields"/>
 </template>
 
 <script setup lang="ts">
+import GameField from '@/components/app/games/lib/GameField.vue'
+
+const fields = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f'],
+        ['i', 'h', 'i'],
+]
 </script>
---
+++ src/components/app/games/lib/GameField.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-row items-start justify-start">
+        <div class="flex flex-col flex-nowrap items-start justify-start border-t border-l border-gray-400">
+            <div class="flex flex-row flex-nowrap items-start justify-start border-b border-gray-400" v-for="row in props.fields">
+                <div class="w-4 h-4 border-r border-gray-400" v-for="item in row">
+                    {{ item }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    fields: string[][] // y, x
+}>()
+</script>
