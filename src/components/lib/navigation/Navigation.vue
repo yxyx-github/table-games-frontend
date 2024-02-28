1311a2048a043085bc9ef6b02de05a4df0aa8f9a
Marvin Waldmann <marvin.waldmann@sap.com>
2024-02-28 09:47:46 +0100

    fix Navigation style on smaller screens

--- src/components/lib/navigation/Navigation.vue
+++ src/components/lib/navigation/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
-    <q-bar class="justify-between">
+    <div class="flex flex-row flex-nowrap items-center justify-between bg-slate-200">
         <div class="flex flex-nowrap flex-row items-stretch justify-start">
             <slot name="start"/>
         </div>
         <div class="flex flex-nowrap flex-row items-stretch justify-end">
             <slot name="end"/>
         </div>
-    </q-bar>
+    </div>
 </template>
 
 <script setup lang="ts">
