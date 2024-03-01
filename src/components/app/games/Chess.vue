163fe9893d083622ec0f6b333879229798d3c28a
Marvin Waldmann <marvin.waldmann@sap.com>
2024-03-01 10:52:04 +0100

    fix Chess board visualization

--- src/components/app/games/Chess.vue
+++ src/components/app/games/Chess.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col flex-nowrap items-stretch gap-2">
         Chess
         <div class="flex flex-row items-start">
-            <GameField :fields="board"
+            <GameField :fields="usedBoard"
                        v-slot="{ item, x, y }"
                        enableClick
                        :clickable="() => true"
@@ -22,19 +22,33 @@
 import GameField from '@/components/app/games/lib/GameField.vue'
 import Icon from '@/components/lib/icons/Icon.vue'
 import { computed } from 'vue'
+import { useChessStore } from '@/stores/games/chess'
+import { ChessGame } from '@/types/games/chess'
+import { ChessPiece } from '@/enums/chessPiece'
+import { useSessionStore } from '@/stores/session'
 
-const board = computed(() =>
+const useSession = useSessionStore()
+const useChess = useChessStore()
+
+const board = computed<string[][]>(() => useChess.state === null ? [] :
         [
-            ['', '1', '2', '3', '4', '5', '6', '7', '8', ''],
-            ...(fields.map((row, index) => {
-                const letter = String.fromCharCode((fields.length - 1 - index) + 65)
-                return [letter, ...row, letter]
-            })),
-            ['', '1', '2', '3', '4', '5', '6', '7', '8', ''],
+            ['', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', ''],
+            ...(useChess.state.board.map((row, index) => [
+                    `${ index + 1 }`,
+                    ...(row.map(field =>
+                            field === null ? ' ' : chessPieceToIconName(field)
+                    )),
+                    `${ index + 1 }`,
+                ]
+            )),
+            ['', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', ''],
         ]
 )
+const reversedBoard = computed<string[][]>(() => board.value.toReversed().map(row => row.toReversed()))
 
-const fields = [
+const usedBoard = computed<string[][]>(() => useSession.session?.user.host ? reversedBoard.value : board.value)
+
+/*const fields = [
     [
         'chess-rook-d',
         'chess-knight-d',
@@ -80,5 +94,36 @@ const fields = [
         'chess-knight-l',
         'chess-rook-l',
     ],
-]
+]*/
+
+function chessPieceToIconName(piece: ChessPiece): string {
+    switch (piece) {
+        case ChessPiece.BLACK_KING:
+            return 'chess-king-d'
+        case ChessPiece.BLACK_QUEEN:
+            return 'chess-queen-d'
+        case ChessPiece.BLACK_BISHOP:
+            return 'chess-bishop-d'
+        case ChessPiece.BLACK_KNIGHT:
+            return 'chess-knight-d'
+        case ChessPiece.BLACK_ROOK:
+            return 'chess-rook-d'
+        case ChessPiece.BLACK_PAWN:
+            return 'chess-pawn-d'
+        case ChessPiece.WHITE_KING:
+            return 'chess-king-l'
+        case ChessPiece.WHITE_QUEEN:
+            return 'chess-queen-l'
+        case ChessPiece.WHITE_BISHOP:
+            return 'chess-bishop-l'
+        case ChessPiece.WHITE_KNIGHT:
+            return 'chess-knight-l'
+        case ChessPiece.WHITE_ROOK:
+            return 'chess-rook-l'
+        case ChessPiece.WHITE_PAWN:
+            return 'chess-pawn-l'
+    }
+}
+
+useChess.loadState()
 </script>
--- src/types/games/chess.d.ts
+++ src/types/games/chess.d.ts
@@ -2,7 +2,7 @@ import { ChessPiece } from '@/enums/chessPiece'
 import { ChessGameState } from '@/enums/chessGameState'
 
 export type ChessGame = {
-    board: ChessPiece[]
+    board: (ChessPiece | null)[][]
     turn: number
     state: ChessGameState
     winner: number | null
