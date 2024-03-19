d043cbc631a2d91dbe4ca44283f4ae60e9e228e0
Marvin Waldmann <marvin.waldmann@sap.com>
2024-03-19 10:37:23 +0100

    remove comment

--- src/components/app/games/lib/GameField.vue
+++ src/components/app/games/lib/GameField.vue
@@ -18,23 +18,6 @@
             </div>
         </template>
     </div>
-<!--    <div class="flex flex-row items-start justify-start">
-        <div class="flex flex-col flex-nowrap items-start justify-start border-t border-l border-gray-400">
-            <div class="flex flex-row flex-nowrap items-stretch justify-start border-b border-gray-400" v-for="(row, y) in props.fields">
-                <div class="flex flex-row items-center justify-start border-r border-gray-400" v-for="(item, x) in row">
-                    <GameFieldItem
-                            :enableClick="props.enableClick"
-                            :clickable="() => props.clickable(item, x, y)"
-                            @click="emit('click', { x, y })"
-                    >
-                        <slot :item="item" :x="x" :y="y">
-                            <Icon :name="item"/>
-                        </slot>
-                    </GameFieldItem>
-                </div>
-            </div>
-        </div>
-    </div>-->
 </template>
 
 <script setup lang="ts">
